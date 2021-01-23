6fcfe1f16d20540192394bef05f3a5202b76703a
Jon Willis <jondwillis@gmail.com>
2021-01-23 16:13:15 -0500

    first commit

---
+++ FirebaseSPMBug/FirebaseSPMBug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseSPMBug
+//
+//  Created by Jonathan Willis on 1/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseSPMBug/FirebaseSPMBug/FirebaseSPMBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSPMBugApp.swift
+//  FirebaseSPMBug
+//
+//  Created by Jonathan Willis on 1/23/21.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseSPMBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
