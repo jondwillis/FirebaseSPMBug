06f4789d7df2de1a86f5162f336f7025ad79290a
Jon Willis <jondwillis@gmail.com>
2021-02-11 22:28:42 -0500

    Xcode 12.5 & explicit package name

--- FirebaseSPMBug/Analytics/Package.swift
+++ FirebaseSPMBug/Analytics/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Quick/Quick.git", from: "3.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "7.4.0"),
+        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk", from: "7.6.0"),
     ],
     targets: [
         .target(
